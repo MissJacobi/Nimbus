7d8af2f779cdd61040ac1d57973ed0c0ab355fca
MissJacobi <feliciajacobi97.fj@gmail.com>
2026-01-28 18:13:26 +0100

    swagger open

--- src/main/java/se/jensen/felicia/nimbus/security/SecurityConfiguration.java
+++ src/main/java/se/jensen/felicia/nimbus/security/SecurityConfiguration.java
@@ -39,7 +39,7 @@ public class SecurityConfiguration {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**","/swagger-ui.html")
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**","/swagger-ui.html").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults());
