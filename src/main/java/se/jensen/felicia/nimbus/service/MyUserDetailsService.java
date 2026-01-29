7374012d2ce4d37752f7d8c6dbabe7b693eaf3a4
MissJacobi <feliciajacobi97.fj@gmail.com>
2026-01-29 14:23:54 +0100

    Update med passwordencoder

--- src/main/java/se/jensen/felicia/nimbus/service/MyUserDetailsService.java
+++ src/main/java/se/jensen/felicia/nimbus/service/MyUserDetailsService.java
@@ -3,6 +3,8 @@ package se.jensen.felicia.nimbus.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import se.jensen.felicia.nimbus.model.User;
 import se.jensen.felicia.nimbus.repository.UserRepository;
@@ -10,10 +12,12 @@ import se.jensen.felicia.nimbus.security.MyUserDetails;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
-        private UserRepository userRepository;
+        private final UserRepository userRepository;
+        private final PasswordEncoder passwordEncoder;
 
-    public MyUserDetailsService(UserRepository userRepository) {
+    public MyUserDetailsService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
         @Override
