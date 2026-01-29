85d737d12084a7b5c6d707567a3b1a1e89980b81
MissJacobi <feliciajacobi97.fj@gmail.com>
2026-01-29 18:19:49 +0100

    token and log in

--- src/main/java/se/jensen/felicia/nimbus/security/SecurityConfiguration.java
+++ src/main/java/se/jensen/felicia/nimbus/security/SecurityConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
